44dfdb80e685533fe8d554b8f0d0a734943ea275
juliajulia89 <julkajanicka@gmail.com>
2021-12-09 11:11:15 +0100

    cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    allowed_formats: ["png", "jpg", "svg", "tiff", "bmp", "jpeg", "gif", "pdf"],
+    folder: "project3", // Folder name on the Cloudinary disk
+  },
+});
+
+module.exports = multer({ storage }); // Multer will be responsible for reading the form and store on the cloud
