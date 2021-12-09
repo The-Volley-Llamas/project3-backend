9f55a1315fede6340e38d152cd36b432addb66ce
Greg Munro <gregmunro90@gmail.com>
2021-12-09 12:41:48 +0100

    seeds updated with tester information

--- seedVenues.js
+++ seedVenues.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGODB = process.env.MONGODB_URI;
-//const Event = require("INSERT MODEL PATH");
+const Venue = require("./models/Venue.model");
 
 // connect to the database
 const seedVenues = async () => {
@@ -13,12 +13,41 @@ const seedVenues = async () => {
     .catch((err) => console.log("Connection failed"));
   //seeding function
   try {
-    const allVenues = {
-      event: ["objectId of populated events"],
-      location: "",
-      rating: "",
-      type: "",
-    };
+    const allVenues = [
+      {
+        event: { type: Schema.Types.ObjectId, ref: "Event" },
+        description: "Artificial surface",
+        location: "Carrer de Margarit 58, 08004",
+        rating: 4.5,
+        image: {
+          type: String,
+          default:
+            "https://lh3.googleusercontent.com/proxy/RtIvYzyAhS6staD8A8omCf8TCXGPU7xJp-OxOQlvcCXP9bKljvFRYrC4VKits23OdgmtqWIAFgZXoSAD_SbC5taceH2aASwAq2BBOEhlQO-_DZarxsYDlycNiST9",
+        },
+      },
+      {
+        event: { type: Schema.Types.ObjectId, ref: "Event" },
+        description: "Natural grass",
+        location: "Carrer del Dr.Aiguader 58, 08003",
+        rating: 4.1,
+        image: {
+          type: String,
+          default:
+            "https://www.barcelonaaldia.com/wp-content/uploads/2020/12/baro_viver_campo.jpg",
+        },
+      },
+      {
+        event: { type: Schema.Types.ObjectId, ref: "Event" },
+        description: "Artificial surface",
+        location: "Avenida del Litoral 85, 08005",
+        rating: 5.0,
+        image: {
+          type: String,
+          default:
+            "https://lh5.googleusercontent.com/p/AF1QipP-_4ygOU_oQWhN6UE0pzk8gYQQD7SI-fMq-dHm=w1080-k-no",
+        },
+      },
+    ];
     const createdEvents = await Venue.create(allVenues);
     console.log(allVenues);
   } catch (error) {
--- seedsEvents.js
+++ seedsEvents.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGODB = process.env.MONGODB_URI;
-//const Event = require("INSERT MODEL PATH");
+const Event = require("./models/Event.model");
 
 // connect to the database
 const seedEvents = async () => {
@@ -13,12 +13,36 @@ const seedEvents = async () => {
     .catch((err) => console.log("Connection failed"));
   //seeding function
   try {
-    const allEvents = {
-      name: "",
-      email: "",
-      password: "",
-      profileImage: "",
-    };
+    const allEvents = [
+      {
+        sport: "Football",
+        numberOfPlayers: 14,
+        user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        venue: [{ type: Schema.Types.ObjectId, ref: "Venue" }],
+        location: "La Satalia",
+        time: 17.0,
+        price: 5,
+      },
+      {
+        sport: "Football",
+        numberOfPlayers: 10,
+        user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        venue: [{ type: Schema.Types.ObjectId, ref: "Venue" }],
+        location: "Club de Fútbol La Catalana",
+        time: 18.0,
+        price: 5,
+      },
+      {
+        sport: "Football",
+        numberOfPlayers: 14,
+        user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        venue: [{ type: Schema.Types.ObjectId, ref: "Venue" }],
+        location: "Agapito Fernandez",
+        time: 19.0,
+        price: 5,
+      },
+    ];
+
     const createdEvents = await Event.create(allEvents);
     console.log(allEvents);
   } catch (error) {
