c465af194cc9fe9da60341e9d56053c3d0904f74
juliajulia89 <julkajanicka@gmail.com>
2021-12-09 10:13:38 +0100

    models done

--- models/User.model.js
+++ models/User.model.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
+  email: { type: String, unique: true, required: [true, "Email is required"] },
+  password: { type: String, required: [true, "Password is required"] },
+  name: { type: String, required: true, required: true },
+  image: {
+    type: String,
+    default:
+      "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+  },
 });
 
 module.exports = model("User", userSchema);
+module.exports = User;
