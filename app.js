b7fbe89500f41aa83478decf72a5d74f302a7a75
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-12-10 10:51:14 +0100

    removed auth

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.use("/api", allRoutes);
 const eventsRouter = require("./routes/event.routes");
 app.use("/api", eventsRouter);
 
-const authRouter = require("./routes/auth.routes");
-app.use("/auth", authRouter);
 
 // const authRouter = require("./routes/auth.routes");
 // app.use("/auth", authRouter);
