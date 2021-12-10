810deec5cd9b8e917df954fdfd8140c4de2896df
juliajulia89 <julkajanicka@gmail.com>
2021-12-10 12:58:42 +0100

    updated models and work on user route

--- models/User.model.js
+++ models/User.model.js
@@ -10,6 +10,7 @@ const userSchema = new Schema({
     default:
       "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
   },
+  event: { type: Schema.Types.ObjectId, ref: "Event" },
 });
 
 module.exports = model("User", userSchema);
